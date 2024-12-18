c333dad99f7cf4b2c5ff63b41ec0d507b248897c
root <root@DESKTOP-6E5CR86.>
2024-12-18 21:43:17 +0200

    21:43 change check

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,10 +1,12 @@
+//  new code to save
+
 import express from 'express';
 
 const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-    res.send('Hello World! yaffa hamalca wwowwww  ');
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
